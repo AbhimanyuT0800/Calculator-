566a3f54ceed830d3b4f698955b6e8be2ee6b605
Abhimanyu <abhimanyua0800@gmail.com>
2022-04-27 20:11:12 +0530

    Almost Calculator Done

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function key(val){
     document.getElementById("screen").value+=val
 }
+function clearfeld(){
+    document.getElementById("screen").value=""
+
+}
+function equal(){
+    var text=document.getElementById("screen").value
+var result=eval(text)
+document.getElementById("screen").value=result
+
+}
